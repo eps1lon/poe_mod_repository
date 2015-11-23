fec0915ed587a02f9107974829ba4dad543652d9
eps1lon <silbermann.sebastian@gmail.com>
2015-11-23 09:54:10 +0100

    added base stats for items

--- js/data/baseitemtypes.sql
+++ js/data/baseitemtypes.sql
@@ -1,1 +1,13 @@
-SELECT `Rows` as `primary`, `Rows`, `Name`, ItemClass, Width, Height, TagsKeys, Implicit_ModsKeys, DropLevel, InheritsFrom FROM baseitemtypes
+SELECT item.`Rows` as `primary`, item.`Rows`, item.`Name`, item.ItemClass, 
+    item.Width, item.Height, item.TagsKeys, item.Implicit_ModsKeys, 
+    item.DropLevel, item.InheritsFrom,
+    weapon.Critical, weapon.Speed, weapon.DamageMin, weapon.DamageMax, weapon.RangeMax,
+    armour.Armour, armour.Evasion, armour.EnergyShield,
+    requirements.ReqStr, requirements.ReqDex, requirements.RegInt as ReqInt
+FROM baseitemtypes item
+LEFT JOIN WeaponTypes weapon
+    ON weapon.BaseItemTypesKey = item.Rows
+LEFT JOIN ComponentArmour armour
+    ON armour.VirtualPath = item.VirtualPath
+LEFT JOIN ComponentAttributeRequirements requirements
+    ON requirements.VirtualPath = item.VirtualPath
