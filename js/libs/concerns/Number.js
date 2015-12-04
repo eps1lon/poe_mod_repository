30e656e235b0e686c12b7d6e6d57147e5d8588e7
eps1lon <silbermann.sebastian@gmail.com>
2015-12-04 13:51:30 +0100

    Update 2015-12-04T12:51:22.501Z

--- js/libs/concerns/Number.js
+++
@@ -1,6 +0,0 @@
-(function (__undefined) {
-    Number.prototype.equals = function (other_number) {
-        return typeof other_number === 'number' 
-                && this.valueOf() === other_number;
-    };
-})();
