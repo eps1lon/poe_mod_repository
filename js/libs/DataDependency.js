1a8e980eeed497794bcb636763625220b9cbc3b5
eps1lon <silbermann.sebastian@gmail.com>
2015-12-06 10:07:30 +0100

    documented DataDependency

--- js/libs/DataDependency.js
+++ js/libs/DataDependency.js
@@ -1,13 +1,30 @@
 /* global this, Class */
 
 (function (__undefined) {
+    /**
+     * class DataDependency
+     * 
+     * class for loading a json data
+     */
     this.DataDependency = Class.extend({
+        /**
+         * 
+         * @param {String} path path to json data
+         * @param {String} loading_indicator jquery selector for loading indicator class
+         * @returns {DataDependency}
+         */
         init: function (path, loading_indicator) {
             this.path = path;
             this.loading_indicator = loading_indicator;
             
             this.state_attr = DataDependency.STATE_ATTR;
         },
+        /**
+         * returns $.getJSON 
+         * 
+         * @param {Function} done callback on $.ajax.done
+         * @returns {$.Dereferred}
+         */
         getJSON: function (done) {
             var that = this;
             $(this.loading_indicator).attr(this.state_attr, DataDependency.STATE.LOADING);
@@ -28,5 +45,8 @@
         FAIL: 3
     };
     
+    /**
+     * default loading state attr
+     */
     this.DataDependency.STATE_ATTR = "data-loading-state";
 })();
